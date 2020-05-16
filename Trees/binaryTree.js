130e02fbbd45e8952f679f510dc4767e575c75c7
ayanrocks <ayanb1999@gmail.com>
2020-05-16 22:50:47 +0530

    added BFS

--- Trees/binaryTree.js
+++ Trees/binaryTree.js
@@ -24,58 +24,67 @@ class BinaryTree {
       this.right = newNode;
     }
   }
+  // BFS
+  breadthFirstSearch() {
+    const queue = [];
+    queue.push(this);
+    let string = "";
 
-  // TODO NOT COMPLETE PRINT 
-
-  print() {
-    let diff = 16;
-    let start = 50;
-    let c = " ";
-
-    thisLevel = [(this, start)];
-
-    while (thisLevel) {
-      let nextLevel = [];
-      let lastLine = "";
-
-      for ((node, d) in thisLevel) {
-        let line = lastLine + c * (d - lastLine.length) + node.data.toString();
-        console.log(line);
-
-        lastLine = line;
-
-        if (node.left) {
-          nextLevel.push((node.left, d - diff));
-        }
-        if (thi.right) nextLevel.push((node.right, d + diff));
-        thisLevel = nextLevel;
-        diff = Math.max(diff);
+    while (queue.length !== 0) {
+      const currentNode = queue.shift();
+      string += currentNode.value + " -> ";
+      if (currentNode.left) {
+        queue.push(currentNode.left);
       }
-      console.log("\n");
+      if (currentNode.right) {
+        queue.push(currentNode.right);
+      }
     }
+    console.log(string);
+  }
+
+  // DFS
+  depthFirstSearch(type) {
+    if (type === "INORDER") return this.preOrderTraversal(this);
+    if (type === "PREORDER") return this.preOrderTraversal(this);
+    if (type === "POSTORDER") return this.postOrderTraversal(this);
+  }
+
+  inOrderTraversal() {
+    
+  }
+  preOrderTraversal() {
+
+  }
+  postOrderTraversal() {
+
   }
 }
 
 //     11
 //   8     16
 // 5   10     18
-
-const tree = new BinaryTree(56);
-console.log(tree.value);
-console.log(tree.left);
-console.log(tree.left);
+// BFS -> 11 -> 8 -> 16 -> 5 -> 10 -> 18
+// DFS
+//  Inorder   -> 5 -> 8 -> 10 -> 11 -> 16 -> 18
+//  PreOrder  -> 11 -> 8 -> 5 -> 10 -> 16 -> 18
+//  PostOrder -> 5 -> 10 -> 8 -> 18 -> 16 -> 11
+const tree = new BinaryTree(11);
+// console.log(tree.value);
+// console.log(tree.left);
+// console.log(tree.left);
 
 tree.insertLeft(8);
 tree.insertRight(16);
 const leftTree = tree.left;
-const rightTree = tree;
+const rightTree = tree.right;
 
 leftTree.insertLeft(5);
 leftTree.insertRight(10);
 
 rightTree.insertRight(18);
-tree.print()
+tree.breadthFirstSearch();
 
 console.log(tree.value);
 console.log(tree.left);
-console.log(tree.left);
+console.log(tree.right);
